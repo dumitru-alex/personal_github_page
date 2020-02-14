44dc97b46d168cce0a9a27a3edf6dee9befe5e38
Alexandru-Paul Dumitru <alexandru.dumitru@broadcom.com>
2020-02-14 16:06:45 +0100

    prettify + update gitignore list

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends React.Component {
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>
-            Welcome!
-          </h1>
-          <p>
-            I know it's boring around here.... at least for now.... Hopefully the revolving atom provides enough entertainment.
-          </p>
-          <p>
-            More content will come soon. I promise!
-          </p>
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://github.com/dumitru-alex"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-          <a
-            className="App-link"
-            href="https://www.linkedin.com/in/alexandru-paul-dumitru/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Linkedin
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1>Welcome!</h1>
+                    <p>
+                        I know it's boring around here.... at least for now....
+                        Hopefully the revolving atom provides enough
+                        entertainment.
+                    </p>
+                    <p>More content will come soon. I promise!</p>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <a
+                        className="App-link"
+                        href="https://github.com/dumitru-alex"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        GitHub
+                    </a>
+                    <a
+                        className="App-link"
+                        href="https://www.linkedin.com/in/alexandru-paul-dumitru/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Linkedin
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
