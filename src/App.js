c9c7b61b0657721bdc4164b3f081de3c0fd64e44
Alexandru-Paul Dumitru <alexandru.dumitru@broadcom.com>
2020-03-05 16:41:13 +0100

    remove commented out code
    Signed-off-by: Alexandru-Paul Dumitru <alexandru.dumitru@broadcom.com>

--- src/App.js
+++ src/App.js
@@ -9,32 +9,6 @@ class App extends React.Component {
         return (
             <div className="App">
                 <Header />
-                {/* <header className="App-header">
-                    <h1>Welcome!</h1>
-                    <p>
-                        I know it's boring around here.... at least for now....
-                        Hopefully the revolving atom provides enough
-                        entertainment.
-                    </p>
-                    <p>More content will come soon. I promise!</p>
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <a
-                        className="App-link"
-                        href="https://github.com/dumitru-alex"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        GitHub
-                    </a>
-                    <a
-                        className="App-link"
-                        href="https://www.linkedin.com/in/alexandru-paul-dumitru/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Linkedin
-                    </a>
-                </header> */}
             </div>
         );
     }
